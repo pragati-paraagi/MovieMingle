fc5b08c0b98d81ff0cf97bd5792cf4cd7a93e545
pragati_paraagi <202251097@iiitvadodara.ac.in>
2024-01-18 20:31:29 +0530

    hi

---
+++ lib/allApi.dart
@@ -0,0 +1,13 @@
+import 'apiKey.dart';
+
+String trendingweekurl = 'https://api.themoviedb.org/3/trending/all/week?api_key=$apikey';
+String trendingdayurl = 'https://api.themoviedb.org/3/trending/all/day?api_key=$apikey';
+String populartvseriesurl = 'https://api.themoviedb.org/3/tv/popular?api_key=$apikey';
+String topratedtvseriesurl = 'https://api.themoviedb.org/3/tv/top_rated?api_key=$apikey';
+String onairtvseriesurl = 'https://api.themoviedb.org/3/tv/on_the_air?api_key=$apikey';
+String popularmoviesurl = 'https://api.themoviedb.org/3/movie/popular?api_key=$apikey';
+String nowplayingmoviesurl = 'https://api.themoviedb.org/3/movie/now_playing?api_key=$apikey';
+String topratedmoviesurl = 'https://api.themoviedb.org/3/movie/top_rated?api_key=$apikey';
+String upcomingmoviesurl = 'https://api.themoviedb.org/3/movie/latest?api_key=$apikey';
+
+//https://api.themoviedb.org/3/trending/all/day?api_key=fc4fb1a9ed20e03ad9ae940967f17266
---
+++ lib/apiKey.dart
@@ -0,0 +1,1 @@
+String apikey = 'fc4fb1a9ed20e03ad9ae940967f17266';
